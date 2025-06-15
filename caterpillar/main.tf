20e69cdc2295a6a309b1f2295311a9c9517cb780
realfabecker <realfabecker@outlook.com>
2025-06-15 17:17:55 -0300

    feat(caterpillar): redeploy

--- caterpillar/main.tf
+++ caterpillar/main.tf
@@ -2,45 +2,6 @@ module "database_dynamodb" {
   source = "./modules/dynamodb"
 }
 
-module "api_gateway" {
-  source = "./modules/api_gateway"
-}
-
-module "lambda_photos" {
-  source = "./modules/lambda_photos"
-
-  function_name = "photos"
-  entrypoint    = "bootstrap"
-
-  zip_source_file = "./projects/photos/backend/out/bootstrap"
-  zip_output_path = "./projects/photos/backend/out"
-  zip_file_name   = "lambda_backend_handler.zip"
-
-  dynamodb_table_arn = module.database_dynamodb.dynamodb_table_arn
-  s3_bucket_name     = "sintese"
-
-  function_url = false
-
-  environment = {
-    APP_NAME            = "photos"
-    DYNAMODB_TABLE_NAME = "sintese"
-    BUCKET_NAME         = "sintese"
-  }
-}
-
-module "photos_proxy" {
-  source = "./modules/api_lambda"
-
-  path_part = "photos"
-
-  lambda_function_name = module.lambda_photos.lambda_function_name
-  lambda_invoke_arn    = module.lambda_photos.lambda_invoke_arn
-
-  rest_api_execution_arn = module.api_gateway.rest_api_execution_arn
-  rest_api_id            = module.api_gateway.rest_api_id
-  rest_root_id           = module.api_gateway.root_resource_id
-}
-
 module "lambda_auth3" {
   source = "./modules/lambda_auth3"
 
@@ -57,7 +18,43 @@ module "lambda_auth3" {
   }
 }
 
-module "auth3_proxy" {
+module "lambda_auth4" {
+  source = "./modules/lambda_auth4"
+
+  function_name = "auth4"
+  entrypoint    = "bootstrap"
+
+  zip_source_file = "../../auth3/auth3/backend/out/bootstrap"
+  zip_output_path = "../../auth3/auth3/backend/out"
+  zip_file_name   = "lambda_backend_handler.zip"
+
+  environment = {
+    COGNITO_JWK_URL = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_63mYbjp1e/.well-known/jwks.json"
+  }
+}
+
+module "lambda_buck3t" {
+  source = "./modules/lambda_buck3t"
+
+  function_name = "buck3t"
+  entrypoint    = "bootstrap"
+
+  zip_source_file = "../../buck3t/buck3t/backend/out/bootstrap"
+  zip_output_path = "../../buck3t/buck3t/backend/out"
+  zip_file_name   = "lambda_backend_handler.zip"
+
+  s3_bucket_name = "sintese"
+
+  environment = {
+    BUCKET_NAME = "sintese"
+  }
+}
+
+module "api_gateway" {
+  source = "./modules/api_gateway"
+}
+
+module "api_auth3" {
   source = "./modules/api_lambda"
 
   path_part = "auth3"
@@ -70,24 +67,7 @@ module "auth3_proxy" {
   rest_root_id           = module.api_gateway.root_resource_id
 }
 
-module "lambda_buck3t" {
-  source = "./modules/lambda_buck3t"
-
-  function_name = "buck3t"
-  entrypoint    = "bootstrap"
-
-  zip_source_file = "./projects/buck3t/backend/out/bootstrap"
-  zip_output_path = "./projects/buck3t/backend/out"
-  zip_file_name   = "lambda_backend_handler.zip"
-
-  s3_bucket_name = "sintese"
-
-  environment = {
-    BUCKET_NAME = "sintese"
-  }
-}
-
-module "buck3t_proxy" {
+module "api_buck3t" {
   source = "./modules/api_lambda"
 
   path_part = "buck3t"
@@ -98,19 +78,4 @@ module "buck3t_proxy" {
   rest_api_execution_arn = module.api_gateway.rest_api_execution_arn
   rest_api_id            = module.api_gateway.rest_api_id
   rest_root_id           = module.api_gateway.root_resource_id
-}
-
-module "lambda_auth4" {
-  source = "./modules/lambda_auth4"
-
-  function_name = "auth4"
-  entrypoint    = "bootstrap"
-
-  zip_source_file = "./projects/auth3/backend/out/bootstrap"
-  zip_output_path = "./projects/auth3/backend/out"
-  zip_file_name   = "lambda_backend_handler.zip"
-
-  environment = {
-    COGNITO_JWK_URL = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_63mYbjp1e/.well-known/jwks.json"
-  }
 }
